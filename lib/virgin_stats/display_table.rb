176ddafe8179d27f0a7f0e9e383d880788f622fe
Tom ten Thij <git@tomtenthij.nl>
2011-01-02 13:57:31 +0000

    Store passed argument as comment, show some more stuff in the table

--- lib/virgin_stats/display_table.rb
+++ lib/virgin_stats/display_table.rb
@@ -1,21 +1,25 @@
 module VirginStats
   class DisplayTable
-    @@format = "%17s | %2s | %9s | %12s | %16s || %2s | %9s | %12s | %17s |"
+    @@format = "%17s | %2s | %9s | %12s | %16s | %6s || %2s | %10s | %12s | %17s | %8s | %6s | %s"
     def self.get_data
       Dir.glob(File.join(VirginStats.data_dir, "*")).map { |f| File.read(f) }
     end
 
     def self.display
-      puts "%17s | %-48s || %-49s |" % [" ", "Up", "Down"]
+      puts "%17s | %-57s || %-58s |" % [" ", "Up", "Down"]
       puts @@format % ["Timestamp",
                        "ID",
                        "Power",
                        "Frequency",
                        "Symbol Rate",
+                       "Mod",
                        "ID",
                        "Power",
                        "Frequency",
-                       "Bit Rate"]
+                       "Bit Rate",
+                       "S2N",
+                       "Mod",
+                       ""]
       get_data.each { |d| puts format_data_point(d) }
     end
 
@@ -27,10 +31,14 @@ module VirginStats
                   data[:upstream]["Power Level"],
                   data[:upstream]["Upstream Frequency"],
                   data[:upstream]["Symbol Rate"],
+                  data[:upstream]["Modulation"],
                   data[:downstream]["Channel ID"],
                   data[:downstream]["Power Level"],
                   data[:downstream]["Downstream Frequency"],
-                  data[:downstream]["Bit Rate"]]
+                  data[:downstream]["Bit Rate"],
+                  data[:downstream]["Signal to Noise Ratio"],
+                  data[:downstream]["Modulation"],
+                  data[:comment]]
     end
   end
 end
