1c86872c6348177b39fdfddad46840c19f587de2
Tom ten Thij <git@tomtenthij.nl>
2011-06-08 01:36:33 +0100

    Show outage duration

--- lib/virgin_stats/network_monitor.rb
+++ lib/virgin_stats/network_monitor.rb
@@ -16,6 +16,7 @@ module VirginStats
     def start
       log("Starting monitoring")
       @last_network_status_online = NetworkMonitor::ping_succeeds?
+      @offline_start_time = Time.now
       @loop_count = 0
       log("Initial status: %s" % (@last_network_status_online ? "online" : "OFFline"))
       while(1)
@@ -23,12 +24,17 @@ module VirginStats
         sleep(@@sleep_duration)
         current_net_status_online = NetworkMonitor::ping_succeeds?
         if current_net_status_online != @last_network_status_online
-          log("Network status changed to: %s" % (current_net_status_online ? "online" : "OFFline"))
-          @last_network_status_online = current_net_status_online
-        else
-          if @loop_count % 1000 == 0
-            log("Monitoring in progress (%d pings performed)" % @loop_count)
+          if current_net_status_online
+            log("Network status changed to: online (outage for %d seconds)" % (Time.now - @offline_start_time))
+          else
+            log("Network status changed to: OFFline")
+            @offline_start_time = Time.now
           end
+          @last_network_status_online = current_net_status_online
+        end
+
+        if @loop_count % 1000 == 0
+          log("Monitoring in progress (%d pings performed)" % @loop_count)
         end
       end
     end
